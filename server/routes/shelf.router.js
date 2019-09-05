813298693b9626bdbd213998915000dad005c4ab
Christopher O'Bannon <Crobwan@gmail.com>
2019-09-05 13:40:02 -0500

    creates GET and POST route in shelf.router.s

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -24,7 +24,17 @@ router.get('/', (req, res) => {
  * Add an item for the logged in user to the shelf
  */
 router.post('/', (req, res) => {
-
+    if(req. isAuthenticated()){
+        console.log('req.user:', req.user);
+        let queryText = `INSERT INTO "item" ("description", "image_url", "user_id") VALUES ($1, $2, $3);`;
+        pool.query(queryText, [req.body.description, req.body.image_url, req.user.id])
+        .then(results => res.sendStatus(201))
+        .catch( error => {
+            console.log('error in server side POST', error);
+        })
+    }else{
+        res.sendStatus(403)
+    }
 });
 
 
@@ -32,7 +42,17 @@ router.post('/', (req, res) => {
  * Delete an item if it's something the logged in user added
  */
 router.delete('/:id', (req, res) => {
-
+    if(req.isAuthenticated()){
+        if(req.body.user_id == req.user.id ){
+            let queryText = `DELETE FROM "item" WHERE "id" = $1`
+            pool.query(queryText, [req.params.id])
+            .then(results => res.sendStatus(201))
+            .catch(error => {
+                console.log('error in server side DELETE', error);
+                res.sendStatus(418)
+            })
+        }else{403}
+    }else{403}
 });
 
 
